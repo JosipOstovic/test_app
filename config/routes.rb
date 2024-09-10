6a65174aa24c76951283b7ba47653a2e5f1b881a
Josip.Ostovic <josip.ostovic00@gmail.com>
2024-09-10 13:17:23 +0200

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   root "pages#home"
+  get "about", to: "pages#about"
 end
